0b48b83e605d65badeb473220c804745a6d69f9a
EduardoC-AMS <caetanotva5252@gmail.com>
2023-05-12 12:51:46 -0300

    Adicionando projeto ao git hub

---
+++ Database.js
@@ -0,0 +1,48 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+async function handleEditPress(){ 
+    const item = await Database.getItem(props.id);
+    props.navigation.navigate("AppForm", item);
+}
+
+async function saveItem(listItem){
+    listItem.id = new Date().getTime();
+    let savedItems = [];
+    const response = await AsyncStorage.getItem('items');
+    
+    if(response) savedItems = JSON.parse(response);
+    savedItems.push(listItem);
+    
+    return AsyncStorage.setItem('items', JSON.stringify(savedItems));
+}
+
+
+
+function getItems(){
+    return AsyncStorage.getItem('items')
+            .then(response => {
+                if(response)
+                    return Promise.resolve(JSON.parse(response));
+                else
+                    return Promise.resolve([]);
+            })
+           
+}
+
+async function getItem(id){
+    const savedItems = await getItems();
+    return savedItems.find(item => item.id === id);
+}
+
+async function deleteItem(id){
+    let savedItems = await getItems();
+    const index = await savedItems.findIndex(item => item.id === id);
+    savedItems.splice(index, 1);
+    return AsyncStorage.setItem('items', JSON.stringify(savedItems));
+}
+
+module.exports = {
+    saveItem,
+    getItems,
+    getItem,
+    deleteItem
+}
